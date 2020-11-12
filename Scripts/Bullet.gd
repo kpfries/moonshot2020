be0578a46bb736074cc8af8fb8dec253d74ae119
Kenny Friesen <kpfries@gmail.com>
2020-11-12 05:45:49 -0500

    give bullets lifetime
    dequeue bullets after 3 second lifetime

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -6,15 +6,12 @@ var vector = Vector2(0, -1)
 var parent_speed: Vector2 = Vector2()
 
 
-
-
 func _ready():
 	rotation = vector.angle()
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-# warning-ignore:unused_argument
 func _physics_process(delta):
 	position += ((speed * vector) + parent_speed) * delta
 
@@ -23,5 +20,8 @@ func _physics_process(delta):
 
 func _on_Bullet_body_entered(body):
 	print(body)
-	#if body.name == "Player":
+	queue_free()
+
+
+func _on_Lifetime_timeout():
 	queue_free()
