df6b7743e69e085ed14673cd820192fdc03d4f38
Kenny Friesen <kpfries@gmail.com>
2020-11-12 05:11:40 -0500

    better bullet behavior
    bullets spawn from the actor shooting them and inherit the shooter's velocity and transform.  Despawn on collision with physics body

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,18 +1,27 @@
 extends Area2D
 
 
-export var speed: float = 500
+export var speed: float = 1000
 var vector = Vector2(0, -1)
+var parent_speed: Vector2 = Vector2()
+
 
 
 
 func _ready():
-	pass # Replace with function body.
+	rotation = vector.angle()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # warning-ignore:unused_argument
 func _physics_process(delta):
-	position.y -= speed * delta * vector.y
-	position.x -= speed * delta * vector.x
+	position += ((speed * vector) + parent_speed) * delta
 
+
+
+
+func _on_Bullet_body_entered(body):
+	print(body)
+	#if body.name == "Player":
+	queue_free()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,8 +4,11 @@ extends RigidBody2D
 export var thrust = 50
 onready var sprite = $Sprite
 export var tether: NodePath = ""
+var bullet_scn := preload("res://Scenes/Projectiles/Bullet.tscn")
+
 onready var ray = $Sprite/RayCast2D
 onready var grapple = get_node(tether)
+onready var gun = $Sprite/Gun
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,16 +26,21 @@ func _process(delta):
 		self.apply_central_impulse(thruster_force)
 		
 	if Input.is_action_just_pressed("grapple"):
-		print(ray.get_collider())
 		if ray.is_colliding() == true:
 			moveto(ray.get_collider(), grapple)
 			grapple.set_node_b(get_path_to(ray.get_collider()))
 			print(ray.get_collider().global_position)
-			print(grapple.global_position)
 	if Input.is_action_just_released("grapple"):
 		if grapple.node_b:
 			grapple.set_node_b(NodePath(""))
 			
+	#eventually check if equipped weapon is repeating and spawn another bullet when elapsed time exceeds firerate but for now we're doing semi auto
+	if Input.is_action_just_pressed("shoot"):
+		var bullet := bullet_scn.instance()
+		bullet.vector = position.direction_to(cursor_pos)
+		bullet.global_transform = gun.global_transform
+		bullet.parent_speed = linear_velocity
+		get_tree().current_scene.add_child(bullet)
 			
 
 func moveto(target, current):
