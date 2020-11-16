63bf0b86af437c605b5e593d091430534a5eed53
Kenny <kpfries@gmail.com>
2020-11-16 00:48:12 -0600

    changed asteroid distribution from random grid plots to poisson-disk sampling

--- Scripts/procedural_pop.gd
+++ Scripts/procedural_pop.gd
@@ -1,25 +1,40 @@
 extends Node2D
 
+export var density = 400
 onready var rng = RandomNumberGenerator.new()
 onready var asteroid_scn := preload("res://Scenes/Scenery/Asteroid.tscn")
 onready var asteroid_rb_scn := preload("res://Scenes/Scenery/Asteroid_rb.tscn")
+onready var level_bounds = $LevelBounds.polygon
+onready var pds = PoissonDiscSampling.new()
+var game_seed: String = ""
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for y in range(14):
-		for x in range(14):
-			var rand = rng.randi_range(1, 10)
-			if rand < 3:
-				var asteroid_rb = asteroid_rb_scn.instance()
-				get_tree().current_scene.add_child(asteroid_rb)
-				asteroid_rb.global_position.y = (y-7) * 400
-				asteroid_rb.global_position.x = (x-7) * 400
-			if rand > 7:
-				var asteroid = asteroid_scn.instance()
-				get_tree().current_scene.add_child(asteroid)
-				asteroid.global_position.y = (y-7) * 400
-				asteroid.global_position.x = (x-7) * 400
+	rng.seed = game_seed.hash()
+	var points = pds.generate_points(density, level_bounds, 3, Vector2(0,0))
+	for point in points: 
+		var asteroid = asteroid_scn.instance()
+		asteroid.position = point
+		get_tree().current_scene.add_child(asteroid)
+		pass
+	
+	
+	
+#	for y in range(14):
+#		for x in range(14):
+#			var rand = rng.randi_range(1, 10)
+#			if rand < 2:
+#				var asteroid_rb = asteroid_rb_scn.instance()
+#				get_tree().current_scene.add_child(asteroid_rb)
+#				asteroid_rb.global_position.y = (y-7) * 400
+#				asteroid_rb.global_position.x = (x-7) * 400
+#			if rand > 8:
+#				var asteroid = asteroid_scn.instance()
+#				get_tree().current_scene.add_child(asteroid)
+#				asteroid.global_position.y = (y-7) * 400
+#				asteroid.global_position.x = (x-7) * 400
 
 
 #func _process(delta):
