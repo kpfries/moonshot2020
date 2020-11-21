6b11ed6c1267d455d26200d1d67904262b3856e4
Kenny Friesen <kpfries@gmail.com>
2020-11-21 17:23:22 -0600

    Enemy Update
    Enemies shoot now

--- Scripts/Enemy_KB.gd
+++ Scripts/Enemy_KB.gd
@@ -7,14 +7,21 @@ export var detection_distance = 700
 export var acceleration: float = 0.2
 
 var velocity: Vector2 = Vector2(0,0)
+
 export var bullet_speed: int
+export var clip_size: int = 10
+var bullets_remaining: int = 30
 var bullet_scn := preload("res://Scenes/Projectiles/Bullet.tscn")
 onready var gun = $EnemyController/Gun
+onready var fire_frequency = $EnemyController/Gun/FireFrequency
+onready var ReloadTimer = $EnemyController/Gun/ReloadTimer
+
 onready var player = get_tree().get_nodes_in_group('Player')[0]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	fire_frequency.start()
 	pass # Replace with function body.
 
 
@@ -22,8 +29,10 @@ func _ready():
 func _physics_process(delta):
 	if player.global_position.distance_to(global_position) < detection_distance:
 		velocity = velocity.linear_interpolate(chase() * speed, acceleration)
+		fire_frequency.paused = false
 	else:
 		velocity = velocity.linear_interpolate(Vector2(0,0), acceleration)
+		fire_frequency.paused = true
 	velocity = move_and_slide(velocity)
 
 
@@ -38,15 +47,14 @@ func chase():
 		look_vec = orth_look_vec.linear_interpolate(std_look_vec, clamp((global_position.distance_to(player.global_position)-targetdistance)/100, 0.0, 1.0))
 	return look_vec
 
-		
-
 
 func shoot():
 	var bullet := bullet_scn.instance()
 	bullet.vector = Vector2(1,0).rotated(rotation)
 	bullet.global_transform = gun.global_transform
-	bullet.parent_speed = Vector2(1,0).rotated(rotation) * velocity
+	bullet.parent_speed = velocity
 	get_tree().current_scene.add_child(bullet)
+	
 
 func damage(dmg):
 	health -= dmg
@@ -56,3 +64,18 @@ func die():
 	#replace with rb corpse with linear_velocity set to velocity
 	print('gottem')
 	queue_free()
+
+
+
+func _on_FireFrequency_timeout():
+	if bullets_remaining > 0:
+		shoot()
+		bullets_remaining -= 1
+		fire_frequency.start()
+	else:
+		ReloadTimer.start()
+
+
+func _on_ReloadTimer_timeout():
+	bullets_remaining = clip_size
+	fire_frequency.start()
