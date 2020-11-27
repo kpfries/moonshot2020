09c87c8db9a3048b7da28bb0493e8091473acb10
Kenny Friesen <kpfries@gmail.com>
2020-11-27 00:39:53 -0600

    Parallax background

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -6,7 +6,6 @@ var vector = Vector2()
 var parent_speed: Vector2 = Vector2(0, 0)
 var damage: int = 1
 
-onready var collision_particle := preload("res://Scenes/Particles/asteroid_collision.tscn")
 
 func _ready():
 	rotation = vector.angle()
@@ -24,10 +23,6 @@ func _physics_process(delta):
 func _on_Bullet_body_entered(body):
 	if body.has_method('damage'):
 		body.damage(damage)
-	if body is StaticBody2D:
-		var particle = collision_particle.instance()
-		particle.global_position = global_position
-		get_tree().current_scene.add_child(particle)
 	print(body)
 	
 	queue_free()
